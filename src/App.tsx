776eae3d9c585c1a8e2648174c578a7228fd79cc
Thaislaa <thaisladaveiga@gmail.com>
2026-03-03 08:05:46 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Provider as ReduxProvider} from "react-redux";
+import { store } from "./store";
+
+export function App() {
+  return(
+    <ReduxProvider store={store}>
+      <h1>Bem vindo ao Redux Toolkit</h1>
+    </ReduxProvider>
+  );
+}
