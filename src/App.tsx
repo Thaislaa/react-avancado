6de70f70a7dd1658a3df23d83634f87772b773c7
Thaislaa <thaisladaveiga@gmail.com>
2026-03-03 08:10:02 -0300

    Corrige exportação do componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Provider as ReduxProvider} from "react-redux";
 import { store } from "./store";
 
-export function App() {
+export default function App() {
   return(
     <ReduxProvider store={store}>
       <h1>Bem vindo ao Redux Toolkit</h1>
